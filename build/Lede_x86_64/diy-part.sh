86def7fc0360a94e740c788cdccc34b8a7fbb435
砼砼 <code@tossp.com>
2021-08-23 17:58:03 +0800

    修改设备默认机器名

--- build/Lede_x86_64/diy-part.sh
+++ build/Lede_x86_64/diy-part.sh
@@ -18,7 +18,7 @@ uci set network.lan.netmask='255.255.255.0'                                 # IP
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
 #uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
 #uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
-uci set system.@system[0].hostname='opnas-lede'                            # 修改主机名称为OpenWrt-123
+uci set system.@system[0].hostname='tsl'                            # 修改主机名称为OpenWrt-123
 EOF
 
 
